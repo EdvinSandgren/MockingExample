f94f832345552256a1013af00bfb15afa1cb0c76
Edvin Sandgren <229709012+EdvinSandgren@users.noreply.github.com>
2026-02-09 23:25:29 +0100

    Adds unit test for getAvailableRooms method.

--- src/test/java/com/example/BookingSystemTest.java
+++ src/test/java/com/example/BookingSystemTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.awt.print.Book;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,23 +15,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BookingSystemTest {
     BookingSystem testSystem;
+    LocalDateTime testStart = LocalDateTime.of(1999, 1, 2, 0, 0);
+    LocalDateTime testEnd = LocalDateTime.of(1999, 1, 2, 23, 59);
+    List<Room> testRoomList = new ArrayList<>();
 
 
     @BeforeEach
     void setUp() {
         TimeProvider testTime = () -> LocalDateTime.of(1999, 1, 1, 0, 0);
 
-        Room testRoom = new Room("1", "test");
+        Room testRoom1 = new Room("1", "test1");
+        Room testRoom2 = new Room("2", "test2");
+        Room testRoom3 = new Room("3", "test3");
+
+        testRoomList.add(testRoom1);
+        testRoomList.add(testRoom2);
+        testRoomList.add(testRoom3);
 
         RoomRepository testRoomRepository = new RoomRepository() {
             @Override
             public Optional<Room> findById(String id) {
-                return Optional.of(testRoom);
+                return Optional.ofNullable(testRoomList.stream().filter(room -> room.getId().equals(id)).findFirst().orElse(null));
             }
 
             @Override
             public List<Room> findAll() {
-                return List.of();
+                return testRoomList;
             }
 
             @Override
@@ -60,18 +70,16 @@ class BookingSystemTest {
     @Test
     void bookRoomTest() {
         String testID = "1";
-        LocalDateTime testStart = LocalDateTime.of(1999, 1, 2, 0, 0);
-        LocalDateTime testEnd = LocalDateTime.of(1999, 1, 2, 23, 59);
 
         Assertions.assertThat(testSystem.bookRoom(testID, testStart, testEnd));
     }
 
     @Test
-    void getAvailableRooms() {
-
+    void getAvailableRoomsTest() {
+        Assertions.assertThat(testSystem.getAvailableRooms(testStart, testEnd).equals(testRoomList));
     }
 
     @Test
-    void cancelBooking() {
+    void cancelBookingTest() {
     }
 }
